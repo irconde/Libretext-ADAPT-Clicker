c11ba2480955fd73d5ea6c31ebcd084af890e606
wanickols01 <wanickols@ualr.edu>
2022-11-12 22:10:15 -0600

    Bottom text move
    moved bottom text slightly down

--- lib/create_account/create_account_widget.dart
+++ lib/create_account/create_account_widget.dart
@@ -6,7 +6,6 @@ import '../flutter_flow/flutter_flow_util.dart';
 import '../login_page/login_page_widget.dart';
 import 'dart:async';
 import 'package:flutter/material.dart';
-import '../gen/assets.gen.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
