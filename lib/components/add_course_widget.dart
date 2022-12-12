2298785bc409760252d7422fc676dff8a1ba0ca7
zpborromeo <zborromeo@ualr.edu>
2022-12-12 09:44:25 -0600

    Updated Widget for Adding Course
    TextField is only issue now

--- lib/components/add_course_widget.dart
+++ lib/components/add_course_widget.dart
@@ -67,168 +67,149 @@ class _AddCourseWidgetState extends State<AddCourseWidget>
           decoration: BoxDecoration(
             color: Color(0x0E1862B3),
           ),
-          alignment: AlignmentDirectional(0, 0),
+          alignment: AlignmentDirectional(0, 1),
           child: InkWell(
             onTap: () async {}, //keeps actual background not clicking
             child: Container(
               width: double.infinity,
-              constraints: BoxConstraints(
-                maxWidth: double.infinity,
-              ),
               decoration: BoxDecoration(
                 color: FlutterFlowTheme.of(context).secondaryBackground,
               ),
               child: Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(32, 0, 32, 0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        SvgPicture.asset(
-                          'assets/images/book_icon.svg',
-                          fit: BoxFit.contain,
-                        ),
-                        Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(4, 8, 0, 0),
-                          child: Text(
-                            'Course Registration',
-                            style: FlutterFlowTheme.of(context)
-                                .bodyText1
-                                .override(
-                                  fontFamily: 'Open Sans',
-                                  fontSize: 20,
-                                  color:
-                                      FlutterFlowTheme.of(context).primaryColor,
-                                  fontWeight: FontWeight.w700,
-                                ),
+                padding: EdgeInsetsDirectional.fromSTEB(32, 32, 32, 32),
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SvgPicture.asset(
+                            'assets/images/book_icon.svg',
+                            width: 32,
+                            height: 32,
+                            color: FlutterFlowTheme.of(context).primaryColor,
+                            fit: BoxFit.fill,
                           ),
-                        )
-                      ],
-                    ),
-                    Divider(
-                      color: FlutterFlowTheme.of(context).secondaryText,
-                    ),
-                    Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(0, 12, 0, 0),
-                      child: Text(
-                        'Please enter the course code used given by your instructor.',
-                        style: FlutterFlowTheme.of(context).bodyText1.override(
-                              fontFamily: 'Open Sans',
-                              fontSize: 14,
-                              color: FlutterFlowTheme.of(context).secondaryText,
-                              fontWeight: FontWeight.w400,
-                            ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(0, 16, 0, 0),
-                      child: TextField(
-                        controller: accessCodeACController,
-                        autofocus: true,
-                        obscureText: false,
-                        decoration: InputDecoration(
-                          labelText: "Course Code",
-                          prefixIcon: Icon(
-                            Icons.mode_edit,
-                            color: FlutterFlowTheme.of(context).secondaryText,
-                          ),
-                        ),
-                        style: FlutterFlowTheme.of(context).bodyText1.override(
-                              fontFamily: 'Open Sans',
-                              fontSize: 14,
-                              color: FlutterFlowTheme.of(context).secondaryText,
-                              fontWeight: FontWeight.w400,
-                            ),
-                        textAlign: TextAlign.start,
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(36, 0, 0, 16),
-                      child: Text(
-                        functions.getTopError(FFAppState().errorsList.toList()),
-                        style: FlutterFlowTheme.of(context).bodyText1.override(
-                              fontFamily: 'Open Sans',
-                              color: FlutterFlowTheme.of(context).failure,
-                              fontSize: 14,
-                              fontWeight: FontWeight.normal,
-                            ),
-                      ).animated(
-                          [animationsMap['textOnActionTriggerAnimation']!]),
-                    ),
-                    Align(
-                      alignment: AlignmentDirectional(0, 0),
-                      child: Padding(
-                        padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 32),
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            textStyle: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w600,
-                              fontFamily: 'Open Sans',
-                            ),
-                            foregroundColor:
-                                FlutterFlowTheme.of(context).primaryBtnText,
-                            fixedSize: const Size(330, 36),
-                            backgroundColor:
-                                FlutterFlowTheme.of(context).primaryColor,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(4),
-                            ),
-                          ),
-                          onPressed: () async {
-                            addCourse = await AddCourseCall.call(
-                              token: FFAppState().authToken,
-                              accessCode: accessCodeACController!.text,
-                              timeZone: 'America/Belize',
-                            );
-                            if ((addCourse?.succeeded ?? true)) {
-                              Navigator.pop(context);
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(
-                                  content: Text(
-                                    'Password Updated Successfully',
-                                    style: TextStyle(
-                                      color: FlutterFlowTheme.of(context)
-                                          .primaryBtnText,
-                                    ),
+                          Padding(
+                            padding: EdgeInsetsDirectional.fromSTEB(8, 0, 0, 0),
+                            child: Text(
+                              'Course Registration',
+                              textAlign: TextAlign.center,
+                              style: FlutterFlowTheme.of(context)
+                                  .bodyText1
+                                  .override(
+                                    fontFamily: 'Open Sans',
+                                    color: FlutterFlowTheme.of(context)
+                                        .primaryColor,
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
                                   ),
-                                  duration: Duration(milliseconds: 4000),
-                                  backgroundColor:
-                                      FlutterFlowTheme.of(context).success,
-                                ),
-                              );
-                            } else {
-                              setState(
-                                  () => FFAppState().errorsList = (getJsonField(
-                                        (addCourse?.jsonBody ?? ''),
-                                        r'''$.errors..*''',
-                                      ) as List)
-                                          .map<String>((s) => s.toString())
-                                          .toList());
-                              if (animationsMap[
-                                      'textOnActionTriggerAnimation'] ==
-                                  null) {
-                                return;
-                              }
-                              await (animationsMap[
-                                          'textOnActionTriggerAnimation']!
-                                      .curvedAnimation
-                                      .parent as AnimationController)
-                                  .forward(from: 0.0);
-                            }
-
-                            setState(() {});
-                          },
-                          child: const Text("JOIN COURSE"),
+                            ),
+                          )
+                        ],
+                      ),
+                      Divider(
+                        height: 48,
+                        thickness: 1,
+                        color: FlutterFlowTheme.of(context).lineColor,
+                      ),
+                      Container(
+                        width: double.infinity,
+                        child: Text(
+                          'Please enter the course code used given by your instructor.',
+                          textAlign: TextAlign.start,
+                          style: FlutterFlowTheme.of(context)
+                              .bodyText1
+                              .override(
+                                fontFamily: 'Open Sans',
+                                fontSize: 14,
+                                color:
+                                    FlutterFlowTheme.of(context).secondaryText,
+                                fontWeight: FontWeight.normal,
+                              ),
                         ),
                       ),
-                    ),
-                  ],
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 24, 0, 24),
+                        child: Container(
+                          width: double.infinity,
+                          child: TextField(
+                            controller: accessCodeACController,
+                            decoration: InputDecoration(
+                              labelText: "Course Code",
+                              prefixIcon: Icon(
+                                Icons.mode_edit,
+                              ),
+                              floatingLabelStyle: TextStyle(
+                                  color: FlutterFlowTheme.of(context)
+                                      .primaryColor),
+                              hintText: 'Course Code',
+                            ),
+                            style: FlutterFlowTheme.of(context).bodyText1,
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size.fromHeight(36),
+                          textStyle: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w600),
+                          primary: FlutterFlowTheme.of(context).primaryColor,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(4),
+                          ),
+                          elevation: 4,
+                        ),
+                        onPressed: () async {
+                          addCourse = await AddCourseCall.call(
+                            token: FFAppState().authToken,
+                            accessCode: accessCodeACController!.text,
+                            timeZone: 'America/Belize',
+                          );
+                          if ((addCourse?.succeeded ?? true)) {
+                            Navigator.pop(context);
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                  'Password Updated Successfully',
+                                  style: TextStyle(
+                                    color: FlutterFlowTheme.of(context)
+                                        .primaryBtnText,
+                                  ),
+                                ),
+                                duration: Duration(milliseconds: 4000),
+                                backgroundColor:
+                                    FlutterFlowTheme.of(context).success,
+                              ),
+                            );
+                          } else {
+                            setState(
+                                () => FFAppState().errorsList = (getJsonField(
+                                      (addCourse?.jsonBody ?? ''),
+                                      r'''$.errors..*''',
+                                    ) as List)
+                                        .map<String>((s) => s.toString())
+                                        .toList());
+                            if (animationsMap['textOnActionTriggerAnimation'] ==
+                                null) {
+                              return;
+                            }
+                            await (animationsMap[
+                                        'textOnActionTriggerAnimation']!
+                                    .curvedAnimation
+                                    .parent as AnimationController)
+                                .forward(from: 0.0);
+                          }
+
+                          setState(() {});
+                        },
+                        child: const Text("JOIN COURSE"),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
