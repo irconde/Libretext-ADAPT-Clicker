8d9a27680fa41cd4b96ef7c1b4f13b938fc7d424
zpborromeo <zborromeo@ualr.edu>
2023-01-09 13:32:34 -0600

    Added Placeholder Image/Icon for Type of Notification + updated Font Styling

--- lib/notifications_page/notifications_page_widget.dart
+++ lib/notifications_page/notifications_page_widget.dart
@@ -5,6 +5,7 @@ import '../flutter_flow/flutter_flow_util.dart';
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class NotificationsPageWidget extends StatefulWidget {
   const NotificationsPageWidget({Key? key}) : super(key: key);
@@ -124,29 +125,39 @@ class _NotificationsPageWidgetState extends State<NotificationsPageWidget> {
                                 enrollmentsList[enrollmentsListIndex];
                             return Padding(
                               padding:
-                                  EdgeInsetsDirectional.fromSTEB(24, 24, 0, 0),
+                                  EdgeInsetsDirectional.fromSTEB(30, 24, 0, 0),
                               child: Column(
                                 mainAxisSize: MainAxisSize.max,
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Text(
-                                    getJsonField(
-                                      enrollmentsListItem,
-                                      r'''$.course_section_name''',
-                                    ).toString(),
-                                    style: FlutterFlowTheme.of(context)
-                                        .bodyText1
-                                        .override(
-                                          fontFamily: 'Open Sans',
-                                          color: FlutterFlowTheme.of(context)
-                                              .primaryColor,
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.w600,
-                                        ),
+                                  Row(
+                                    children: [
+                                      Padding(
+                                        padding: EdgeInsetsDirectional.fromSTEB(0, 0, 5, 0),
+                                        child: SvgPicture.asset('assets/images/book_icon.svg',
+                                          height: 24,
+                                          width: 24,),
+                                      ),
+                                      Text(
+                                        getJsonField(
+                                          enrollmentsListItem,
+                                          r'''$.course_section_name''',
+                                        ).toString(),
+                                        style: FlutterFlowTheme.of(context)
+                                            .bodyText1
+                                            .override(
+                                              fontFamily: 'Open Sans',
+                                              color: FlutterFlowTheme.of(context)
+                                                  .primaryColor,
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                      ),
+                                    ],
                                   ),
                                   Padding(
                                     padding: EdgeInsetsDirectional.fromSTEB(
-                                        0, 8, 0, 8),
+                                        30, 8, 0, 24),
                                     child: Text(
                                       getJsonField(
                                         enrollmentsListItem,
@@ -162,19 +173,10 @@ class _NotificationsPageWidgetState extends State<NotificationsPageWidget> {
                                           ),
                                     ),
                                   ),
-                                  Text(
-                                    getJsonField(
-                                      enrollmentsListItem,
-                                      r'''$.id''',
-                                    ).toString(),
-                                    style: FlutterFlowTheme.of(context)
-                                        .bodyText1
-                                        .override(
-                                          fontFamily: 'Open Sans',
-                                          color: FlutterFlowTheme.of(context)
-                                              .secondaryText,
-                                          fontWeight: FontWeight.normal,
-                                        ),
+                                  Divider(
+                                    height: 1,
+                                    thickness: 1,
+                                    endIndent: 30,
                                   ),
                                 ],
                               ),
