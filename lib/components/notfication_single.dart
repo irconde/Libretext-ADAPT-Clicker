8b6ad25b126d5ca8f3b2df862e2bff0c2aecd315
zpborromeo <zborromeo@ualr.edu>
2023-02-24 15:10:24 -0600

    Single Notification Delete 1
    - Updated swiping motion to delete icon
    - Wrapped Notification block in Dismissible Widget
    -BUG: Notification not being removed from the list

--- lib/components/notfication_single.dart
+++ lib/components/notfication_single.dart
@@ -22,62 +22,80 @@ class _NotificationSingleWidgetState extends State<NotificationSingle> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding:
-      EdgeInsetsDirectional.fromSTEB(30, 24, 0, 0),
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(0, 0, 5, 0),
-                child: SvgPicture.asset('assets/images/book_icon.svg',
-                  height: 24,
-                  width: 24,),
-              ),
-              Text(
+    return Dismissible(
+      key: Key(enrollmentsListItem.toString()),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          child: Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: Icon(Icons.delete),
+          ),
+          alignment: Alignment.centerRight,
+        ),
+      ),
+      // onDismissed: (_) {
+      //   setState(() {
+      //     //TODO - Fix onDismissed to remove item from list
+      //   });
+      // },
+      child: Padding(
+        padding:
+        EdgeInsetsDirectional.fromSTEB(30, 24, 0, 0),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 0, 5, 0),
+                  child: SvgPicture.asset('assets/images/book_icon.svg',
+                    height: 24,
+                    width: 24,),
+                ),
+                Text(
+                  getJsonField(
+                    enrollmentsListItem,
+                    r'''$.course_section_name''',
+                  ).toString(),
+                  style: FlutterFlowTheme.of(context)
+                      .bodyText1
+                      .override(
+                    fontFamily: 'Open Sans',
+                    color: FlutterFlowTheme.of(context)
+                        .primaryColor,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(
+                  30, 8, 0, 24),
+              child: Text(
                 getJsonField(
                   enrollmentsListItem,
-                  r'''$.course_section_name''',
+                  r'''$.instructor''',
                 ).toString(),
                 style: FlutterFlowTheme.of(context)
                     .bodyText1
                     .override(
                   fontFamily: 'Open Sans',
                   color: FlutterFlowTheme.of(context)
-                      .primaryColor,
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
+                      .secondaryText,
+                  fontWeight: FontWeight.normal,
                 ),
               ),
-            ],
-          ),
-          Padding(
-            padding: EdgeInsetsDirectional.fromSTEB(
-                30, 8, 0, 24),
-            child: Text(
-              getJsonField(
-                enrollmentsListItem,
-                r'''$.instructor''',
-              ).toString(),
-              style: FlutterFlowTheme.of(context)
-                  .bodyText1
-                  .override(
-                fontFamily: 'Open Sans',
-                color: FlutterFlowTheme.of(context)
-                    .secondaryText,
-                fontWeight: FontWeight.normal,
-              ),
             ),
-          ),
-          Divider(
-            height: 1,
-            thickness: 1,
-            endIndent: 30,
-          ),
-        ],
+            Divider(
+              height: 1,
+              thickness: 1,
+              endIndent: 30,
+            ),
+          ],
+        ),
       ),
     );
 
