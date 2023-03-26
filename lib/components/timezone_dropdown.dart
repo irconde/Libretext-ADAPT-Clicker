633ddb930d53f377193a97a2dd183b35dae9d084
irconde <irconde@ualr.edu>
2023-03-25 22:56:23 -0500

    Comments deleted

--- lib/components/timezone_dropdown.dart
+++ lib/components/timezone_dropdown.dart
@@ -10,7 +10,7 @@ class TimezoneDropdown extends StatefulWidget {
   String? timezoneDropDownValue;
 
   @override
-  State<StatefulWidget> createState() => TimezoneDropdownState();
+  State<StatefulWidget> createState() => TimezoneDropdownState(timezoneDropDownValue);
 }
 
 Future<void> fetchTimezone() async {
@@ -29,66 +29,71 @@ Future<void> fetchTimezone() async {
 
 class TimezoneDropdownState extends State<TimezoneDropdown> {
   String? timezoneDropDownValue;
-
-  @override
-  void initState() {
-    super.initState();
-  }
+  TimezoneDropdownState(this.timezoneDropDownValue);
 
   Future<void> timezoneInit() async {
     await fetchTimezone();
-
-    if (timezoneDropDownValue == null)
-      timezoneDropDownValue = AppState.timezoneContainer!.textzones.first;
   }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: timezoneInit(),
-        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-          return Container(
-            decoration: BoxDecoration(
-              border: Border.all(
-                width: 1,
-                color: FlutterFlowTheme.of(context).textFieldBorder,
-              ),
-              color: FlutterFlowTheme.of(context).textFieldBackground,
+      future: timezoneInit(),
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        return Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: FlutterFlowTheme
+                  .of(context)
+                  .textFieldBorder,
             ),
-            child: ButtonTheme(
-              alignedDropdown: true,
-              child: DropdownButton<String>(
-                value: timezoneDropDownValue,
-                isExpanded: true,
-                items:
-                    AppState.timezoneContainer?.textzones.map((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(
-                      value,
-                      style: FlutterFlowTheme.of(context).bodyText1,
-                    ),
-                  );
-                }).toList(),
-                dropdownColor: FlutterFlowTheme.of(context).textFieldBackground,
-                onChanged: (String? value) {
-                  // This is called when the user selects an item.
-                  setState(() {
-                    timezoneDropDownValue = value;
-                    AppState.userTimezone!.setText(value.toString());
-                    AppState.userTimezone!.setValue(
-                        AppState.timezoneContainer!.getValue(value.toString()));
-                  });
-                },
-                style: FlutterFlowTheme.of(context).bodyText1,
-                underline: Container(),
-                hint: Text(
-                  AppState.userTimezone!.text,
-                  style: FlutterFlowTheme.of(context).bodyText1,
-                ),
+            color: FlutterFlowTheme
+                .of(context)
+                .textFieldBackground,
+          ),
+          child: ButtonTheme(
+            alignedDropdown: true,
+            child: DropdownButton<String>(
+              value: timezoneDropDownValue,
+              isExpanded: true,
+              items: AppState.timezoneContainer?.textzones.map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(
+                    value,
+                    style: FlutterFlowTheme
+                        .of(context)
+                        .bodyText1,
+                  ),
+                );
+              }).toList(),
+              dropdownColor: FlutterFlowTheme
+                  .of(context)
+                  .textFieldBackground,
+              onChanged: (String? value) {
+                // This is called when the user selects an item.
+                setState(() {
+                  timezoneDropDownValue = value;
+                  AppState.userTimezone!.setText(value.toString());
+                  AppState.userTimezone!.setValue(
+                      AppState.timezoneContainer!.getValue(value.toString()));
+                });
+              },
+              style: FlutterFlowTheme
+                  .of(context)
+                  .bodyText1,
+              underline: Container(),
+              hint: Text(
+                AppState.userTimezone!.text,
+                style: FlutterFlowTheme
+                    .of(context)
+                    .bodyText1,
               ),
             ),
-          );
-        });
+          ),
+        );
+      }
+    );
   }
 }
