99e4fa341a91c56bbd1a32da02780b2d1a86eeff
irconde <irconde@ualr.edu>
2023-05-23 15:10:01 -0500

    Accept submission with keyboard of code request

--- lib/components/add_course_widget.dart
+++ lib/components/add_course_widget.dart
@@ -26,7 +26,14 @@ class _AddCourseWidgetState extends ConsumerState<AddCourseWidget>
   void initState() {
     super.initState();
     requiredFields = [code];
-    formValues[code] = [null, null];
+    formFields = [code];
+    initFormFieldsInfo();
+  }
+
+  @override
+  void dispose() {
+    disposeFocusNodes();
+    super.dispose();
   }
 
   void _submit() async {
@@ -73,7 +80,7 @@ class _AddCourseWidgetState extends ConsumerState<AddCourseWidget>
 
   void _onTextChanged(String text) {
     setState(() {
-      formValues[code] = [text, null];
+      formValues[code] = [text, null, formValues[code][focusNodeIndex]];
     });
     checkFormIsReadyToSubmit();
   }
@@ -82,104 +89,111 @@ class _AddCourseWidgetState extends ConsumerState<AddCourseWidget>
   Widget build(BuildContext context) {
     var theme = FlutterFlowTheme.of(context);
     return BlurredBottomSheet(
-              child: Padding(
-                padding: const EdgeInsetsDirectional.fromSTEB(32, 32, 32, 32),
-                child: SingleChildScrollView(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Row(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          SvgPicture.asset(
-                            'assets/images/book_icon.svg',
-                            width: 32,
-                            height: 32,
-                            color: theme.primaryColor,
-                            fit: BoxFit.fill,
-                          ),
-                          Padding(
-                            padding: const EdgeInsetsDirectional.fromSTEB(
-                                8, 0, 0, 0),
-                            child: Text(
-                              'Course Registration',
-                              textAlign: TextAlign.center,
-                              style: theme.bodyText1.override(
-                                fontFamily: 'Open Sans',
-                                color: theme.primaryColor,
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          )
-                        ],
+      child: Padding(
+        padding: const EdgeInsetsDirectional.fromSTEB(32, 32, 32, 32),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SvgPicture.asset(
+                    'assets/images/book_icon.svg',
+                    width: 32,
+                    height: 32,
+                    color: theme.primaryColor,
+                    fit: BoxFit.fill,
+                  ),
+                  Padding(
+                    padding: const EdgeInsetsDirectional.fromSTEB(8, 0, 0, 0),
+                    child: Text(
+                      'Course Registration',
+                      textAlign: TextAlign.center,
+                      style: theme.bodyText1.override(
+                        fontFamily: 'Open Sans',
+                        color: theme.primaryColor,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
                       ),
-                      Divider(
-                        height: 48,
-                        thickness: 1,
-                        color: theme.lineColor,
-                      ),
-                      SizedBox(
-                        width: double.infinity,
-                        child: Text(
-                          'Please enter the course code used given by your instructor.',
-                          textAlign: TextAlign.start,
-                          style: theme.bodyText1.override(
-                            fontFamily: 'Open Sans',
-                            fontSize: 14,
-                            color: theme.secondaryText,
-                            fontWeight: FontWeight.normal,
-                          ),
-                        ),
-                      ),
-                      Form(
-                        child: Padding(
-                          padding:
-                              const EdgeInsetsDirectional.fromSTEB(0, 24, 0, 0),
-                          child: Column(
-                            children: [
-                              SizedBox(
-                                width: double.infinity,
-                                child: TextFormField(
-                                  decoration: InputDecoration(
-                                    labelText: 'Course Code',
-                                    prefixIcon: const Icon(
-                                      Icons.mode_edit,
-                                    ),
-                                    floatingLabelStyle:
-                                        TextStyle(color: theme.primaryColor),
-                                    hintText: 'Course Code',
-                                    errorText: submitted
-                                        ? formValues[code][errorIndex]
-                                        : null,
-                                  ),
-                                  style: theme.bodyText1,
-                                  onChanged: _onTextChanged,
-                                  enabled:
-                                      formState != FormStateValue.processing,
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsetsDirectional.fromSTEB(
-                                    0, 24, 0, 0),
-                                child: CustomElevatedButton(
-                                  formState: formState,
-                                  normalText: 'JOIN COURSE',
-                                  errorText: 'TRY IT AGAIN',
-                                  processingText: 'JOINING COURSE',
-                                  onPressed: _submit,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      )
-                    ],
+                    ),
+                  )
+                ],
+              ),
+              Divider(
+                height: 48,
+                thickness: 1,
+                color: theme.lineColor,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: Text(
+                  'Please enter the course code used given by your instructor.',
+                  textAlign: TextAlign.start,
+                  style: theme.bodyText1.override(
+                    fontFamily: 'Open Sans',
+                    fontSize: 14,
+                    color: theme.secondaryText,
+                    fontWeight: FontWeight.normal,
                   ),
                 ),
               ),
+              Form(
+                child: Padding(
+                  padding: const EdgeInsetsDirectional.fromSTEB(0, 24, 0, 0),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        width: double.infinity,
+                        child: TextFormField(
+                          autofocus: true,
+                          focusNode: formValues[code][focusNodeIndex],
+                          decoration: InputDecoration(
+                            labelText: 'Course Code',
+                            prefixIcon: const Icon(
+                              Icons.mode_edit,
+                            ),
+                            floatingLabelStyle:
+                                TextStyle(color: theme.primaryColor),
+                            hintText: 'Course Code',
+                            errorText:
+                                submitted ? formValues[code][errorIndex] : null,
+                          ),
+                          style: theme.bodyText1,
+                          onChanged: _onTextChanged,
+                          enabled: formState != FormStateValue.processing,
+                          textInputAction: TextInputAction.send,
+                          onFieldSubmitted: (_) {
+                            if (formState != FormStateValue.unfilled) {
+                              _submit();
+                            } else {
+                              FocusScope.of(context).requestFocus(
+                                  formValues[code][focusNodeIndex]);
+                            }
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(0, 24, 0, 0),
+                        child: CustomElevatedButton(
+                          formState: formState,
+                          normalText: 'JOIN COURSE',
+                          errorText: 'TRY IT AGAIN',
+                          processingText: 'JOINING COURSE',
+                          onPressed: _submit,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
