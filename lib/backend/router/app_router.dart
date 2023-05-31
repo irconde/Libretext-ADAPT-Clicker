06b04b40670be8e4324848dbe1797e756f2021ea
irconde <irconde@ualr.edu>
2023-05-30 23:45:51 -0500

    lib/backend/router/app_router.dart commented

--- lib/backend/router/app_router.dart
+++ lib/backend/router/app_router.dart
@@ -6,6 +6,7 @@ import 'package:flutter/widgets.dart';
 import '../user_stored_preferences.dart';
 import '../api_requests/api_calls.dart';
 
+/// AppRouter class that extends the generated router class.
 @AutoRouterConfig(replaceInRouteName: 'Page,Route')
 class AppRouter extends $AppRouter {
   @override
@@ -26,9 +27,10 @@ class AppRouter extends $AppRouter {
   ];
 }
 
+/// RouteHandler class for handling navigation and data retrieval.
 class RouteHandler {
+  /// Navigates to a specified route with optional arguments.
   void navigateTo(BuildContext context, String route, String args) {
-    //print("Route: $route, Args: $args");
     if (route.isNotEmpty) {
       try {
         if (args.isNotEmpty) {
@@ -43,20 +45,23 @@ class RouteHandler {
     }
   }
 
+  /// Retrieves the summary of an assignment.
   Future<String> getSummary(String id) async {
     dynamic assignmentSummary = await GetAssignmentSummaryCall.call(
         token: UserStoredPreferences.authToken, assignmentNum: int.parse(id));
     return jsonEncode(assignmentSummary.jsonBody['assignment']);
   }
 
-
+  /// Retrieves the question for a specific assignment.
   Future<String> getQuestion(List<String> args) async {
     dynamic courseCall = await ViewCall.call(
-        token: UserStoredPreferences.authToken, assignmentID: int.parse(args[1]));
+        token: UserStoredPreferences.authToken,
+        assignmentID: int.parse(args[1]));
     courseCall.jsonBody['course'].remove('textbook_url');
     return jsonEncode(courseCall.jsonBody['course']);
   }
 
+  /// Retrieves the arguments based on the provided path and arguments list.
   Future<String> getArgs(String path, List<String> args) async {
     switch (path) {
       case '/Assignment':
