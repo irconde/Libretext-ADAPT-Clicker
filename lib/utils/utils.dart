1fe8bebaee8da6006bfe382728f4e5eca4159b9d
wanickols01 <wanickols@ualr.edu>
2024-02-23 11:38:20 -0600

    Universal Links Fix
    Changed Android Universal links to be in line with web links.

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -120,7 +120,7 @@ Future<void> mLaunchUrl(String url) async {
   var uri = Uri.parse(url);
   try {
     await canLaunchUrl(uri);
-    await launchUrl(uri, mode: LaunchMode.externalNonBrowserApplication);
+    await launchUrl(uri, mode: LaunchMode.externalApplication);
   } catch (e) {
     logger.e('Could not launch $uri: $e');
   }
